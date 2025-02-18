99cd830e39c55a7d062c0841f95dfc4453069293
otiliadanet <otilia.danet@gmail.com>
2025-02-18 19:10:35 +0200

    Initial commit - Cypress project setup

---
+++ cypress/e2e/sample.cy.js
@@ -0,0 +1,7 @@
+describe('Visit saucedemo', () => {
+    it('Visit the saucedemo website', () => {
+      cy.visit('https://www.saucedemo.com/'); 
+    
+    });
+  });
+  
---
+++ cypress/e2e/saucedemo.cy.js
@@ -0,0 +1,45 @@
+//Login into saucedemo and add 3 products to the cart -> Validate cart content (at least 3 products are in the cart) -> Remove 1 product -> Order
+
+// npx cypress open
+
+describe('SauceDemo automation test', () => {
+    beforeEach(() => {
+      cy.visit('https://www.saucedemo.com/'); 
+    });
+  
+    it('Logins, adds 3 items, removes 1 and places the order', () => {
+      cy.get('[id="user-name"]').type('standard_user'); 
+      cy.get('[id="password"]').type('secret_sauce'); 
+      cy.get('[id="login-button"]').click(); 
+  
+      cy.url().should('include', '/inventory.html');  //check that login was successful
+
+      cy.get('[id="add-to-cart-sauce-labs-backpack"]').click();    //add 1 backpack
+      cy.get('[id="add-to-cart-sauce-labs-bike-light"]').click();  //add 1 bike light
+      cy.get('[id="add-to-cart-test.allthethings()-t-shirt-(red)"]').click();  //add 1 orange blouse
+
+      //verify the 3 items were added to the cart:
+      cy.get('.shopping_cart_badge').should('have.text', '3');  // .shopping_cart_badge means class shopping_cart_badge
+
+      //remove the backpack:
+      cy.get('[id="remove-sauce-labs-backpack"]').click();
+      cy.get('.shopping_cart_badge').should('have.text', '2'); // verify that it remained only 2 items in the cart
+
+
+      //place the order:
+      cy.get('[data-test="shopping-cart-badge"]').click(); //enter shopping cart
+    
+      cy.get('[id="checkout"]').click();  //click on the Checkout button
+
+      cy.get('[id="first-name"]').type('Otilia'); 
+      cy.get('[id="last-name"]').type('Danet'); 
+      cy.get('[id="postal-code"]').type('300066');
+    
+      cy.get('[id="continue"]').click(); //click on the Continue button
+
+      cy.get('[id="finish"]').click();   //click on the Finish button
+
+    });
+
+  });
+  
